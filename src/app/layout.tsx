a8234cea8b64087f52ec3d986c5db95d4a6cdb3f
Nick Martinez <nickmartinezmeakins@gmail.com>
2024-09-27 12:42:16 +0100

    feature/AJBELL1 - updated readme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,4 @@
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function RootLayout({
   children,
@@ -18,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <div className={`${geistSans.variable} ${geistMono.variable} antialiased py-10 bg-[#f4f4f4]`} >
+      <div className="antialiased py-10 bg-[#f4f4f4]" >
         <div className="container mx-auto">
         {children}
         </div>
